94b65dc164bfaa92bbbc85d9fab0ec84e3671a37
Taíbe Cruz <107949509+tandreycruz@users.noreply.github.com>
2025-03-22 00:46:34 -0300

    Add files via upload

---
+++ UtilityDM112/src/main/java/br/inatel/dm112/adapter/MailData.java
@@ -0,0 +1,108 @@
+package br.inatel.dm112.adapter;
+
+public class MailData
+{
+	private int orderNumber;
+
+	private String from;
+
+	private String password;
+
+	private String sendgridAPIKey;
+
+	private String to;
+
+	private byte[] content;
+
+	private String mensagem;
+
+	public MailData()
+	{
+	}
+
+	public MailData(int orderNumber, String from, String password, String to, byte[] content, String mensagem)
+	{
+		this.orderNumber = orderNumber;
+		this.from = from;
+		this.password = password;
+		this.to = to;
+		this.content = content;
+		this.mensagem = mensagem;
+	}
+
+	public int getOrderNumber()
+	{
+		return orderNumber;
+	}
+
+	public void setOrderNumber(int orderNumber)
+	{
+		this.orderNumber = orderNumber;
+	}
+
+	public String getFrom()
+	{
+		return from;
+	}
+
+	public void setFrom(String from)
+	{
+		this.from = from;
+	}
+
+	public String getPassword()
+	{
+		return password;
+	}
+
+	public void setPassword(String password)
+	{
+		this.password = password;
+	}
+
+	public String getTo()
+	{
+		return to;
+	}
+
+	public void setTo(String to)
+	{
+		this.to = to;
+	}
+
+	public byte[] getContent()
+	{
+		return content;
+	}
+
+	public void setContent(byte[] content)
+	{
+		this.content = content;
+	}
+
+	public String getSendgridAPIKey()
+	{
+		return sendgridAPIKey;
+	}
+
+	public void setSendgridAPIKey(String sendgridAPIKey)
+	{
+		this.sendgridAPIKey = sendgridAPIKey;
+	}
+
+	public String getMensagem()
+	{
+		return mensagem;
+	}
+
+	public void setMensagem(String mensagem)
+	{
+		this.mensagem = mensagem;
+	}
+
+	@Override
+	public String toString()
+	{
+		return "MailData [orderNumber=" + orderNumber + ", from=" + from + ", to=" + to + "]";
+	}
+}
